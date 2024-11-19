7e754db0ecf3c9c0f8865b596df5350cdbe4675b
Vlad Osipov <vladyslav.osip@gmail.com>
2024-11-19 12:03:23 +0200

    fix some bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,7 @@ import Profile from "./Profile/Profile";
 import friends from "../friends.json";
 import FriendList from "./FriendList/FriendList";
 import transactions from "../transactions.json";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-
+import TransactionHistory from "./TransactionHistory/TransactionHistory"
 
 const App = () => {
   return (
